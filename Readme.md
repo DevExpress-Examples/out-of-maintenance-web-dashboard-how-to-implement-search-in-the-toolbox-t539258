8dbefbc6d8268c439587ac5ba494ae2a5f0c9e47
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Web Dashboard - How to implement search in the Toolbox
+
+
+<p>This example demonstrates how to implement the search functionality in Web Dashboard's Toolbox. <br>A custom extension is created based on the approach from the <a href="https://www.devexpress.com/Support/Center/p/T466716">T466716: Web Dashboard - How to work with extensions</a> KB article.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/web-dashboard-how-to-implement-search-in-the-toolbox-t539258/17.1.4+/media/6a977c63-e2aa-47cc-9aeb-dded131ad2a7.png"></p>
+
+<br/>
+
+
---
+++ VB/WebApplication18/Default.aspx.vb
@@ -0,0 +1,18 @@
+﻿Imports DevExpress.DashboardWeb
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Namespace WebApplication18
+    Partial Public Class [Default]
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim newDashboardStorage As New DashboardFileStorage("~/App_Data/Dashboards")
+            ASPxDashboard1.SetDashboardStorage(newDashboardStorage)
+        End Sub
+    End Class
+End Namespace
